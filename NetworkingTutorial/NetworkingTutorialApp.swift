278963c0297f95e0b0d3e34ca5a947b37666d779
mia <miabenn74@gmail.com>
2024-07-11 23:13:16 +1000

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Mia on 11/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

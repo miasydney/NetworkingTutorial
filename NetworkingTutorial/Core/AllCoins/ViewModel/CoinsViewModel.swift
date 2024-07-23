3941418516019cab432490aa00543d9f4d685c89
mia <miabenn74@gmail.com>
2024-07-23 16:27:58 +1000

    Threading explanation

--- NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -14,16 +14,19 @@ class CoinsViewModel: ObservableObject {
     
     init() {
         fetchPrice(coin: "ethereum")
+        fetchPrice(coin: "litecoin")
     }
     
     func fetchPrice(coin: String) {
+        print(Thread.current)
+        
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         
         print("Fetching price...") // 1
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            
+            print(Thread.current)
             print("Did receive data: \(data)") // 2
             
             guard let data = data else { return }
@@ -37,6 +40,7 @@ class CoinsViewModel: ObservableObject {
             guard let price = value["usd"] else { return }
             
             DispatchQueue.main.async {
+                print(Thread.current)
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
